1890037c7a31fa845a5331a0fd570ba5d694f9a1
Irhe Jo <kingslanding@naver.com>
2022-01-25 23:33:38 +0900

    Learn middleware and controller

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>I still love you.</h1>");
+  return res.send("I love middlewares.");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleHome);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
