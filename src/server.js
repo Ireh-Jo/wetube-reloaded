846ffa39509ad98101fd3e686da4bdf3de15bd9e
Irhe Jo <kingslanding@naver.com>
2022-01-25 22:59:38 +0900

    http request response

--- src/server.js
+++ src/server.js
@@ -5,10 +5,15 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-    console.log(req);
+  return res.send("<h1>I still love you.</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
 };
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
