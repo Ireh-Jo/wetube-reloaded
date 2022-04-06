98b5f9be9af8dedad900cfd7e785178d4f5aa0c0
Irhe Jo <kingslanding@naver.com>
2022-04-06 23:52:44 +0900

    Video recorder

---
+++ src/client/js/recorder.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+const handleStart = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+  video.srcObject = stream;
+  video.onplay();
+};
+
+startBtn.addEventListener("click", handleStart);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: { main: "./src/client/js/main.js", videoPlayer: "./src/client/js/videoPlayer.js" },
+  entry: { main: "./src/client/js/main.js", videoPlayer: "./src/client/js/videoPlayer.js", recorder: "./src/client/js/recorder.js" },
   mode: "development",
   watch: true,
   plugins: [
