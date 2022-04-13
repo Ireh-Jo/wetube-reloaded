f0bc9079a7e1c0b69bec0da964367256c62589e2
Irhe Jo <kingslanding@naver.com>
2022-04-14 00:07:01 +0900

    댓글 DB에 저장

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -4,14 +4,21 @@ const form = document.getElementById("commentForm");
 const handleSubmit = (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
-  const text = textarea.value;
+  const text = textarea.value.trim();
   const videoId = videoContainer.dataset.id;
+  if (text === "") {
+    return;
+  }
   fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
-    body: {
-      text,
+    headers: {
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      text,
+    }),
   });
+  textarea.value = "";
 };
 
 if (form) {
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 /*
 //callback 방식
@@ -124,8 +125,20 @@ export const registerView = async (req, res) => {
   return res.sendStatus(200);
 };
 
-export const createComment = (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
-  return res.end();
+export const createComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { text },
+    session: { user },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  return res.sendStatus(201);
 };
