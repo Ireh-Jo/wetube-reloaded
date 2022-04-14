c1be95286cbe3ae773d61f810954855c419901cf
Irhe Jo <kingslanding@naver.com>
2022-04-15 01:33:32 +0900

    댓글 삭제

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,26 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const removeBtns = document.querySelectorAll(".video__comment-removeBtn");
 
-const handleSubmit = (event) => {
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const removeBtn = document.createElement("span");
+  removeBtn.innerText = "❌";
+  removeBtn.addEventListener("click", handleClickRemoveBtn);
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(removeBtn);
+  videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value.trim();
@@ -9,7 +28,7 @@ const handleSubmit = (event) => {
   if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,8 +38,26 @@ const handleSubmit = (event) => {
     }),
   });
   textarea.value = "";
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
+};
+
+const handleClickRemoveBtn = async (event) => {
+  const comment = event.target.parentNode;
+  const commentId = comment.dataset.id;
+  const { status } = await fetch(`/api/comments/${commentId}`, {
+    method: "DELETE",
+  });
+  if (status === 200) {
+    comment.remove();
+  }
 };
 
 if (form) {
   form.addEventListener("submit", handleSubmit);
+  removeBtns.forEach((removeBtn) => {
+    removeBtn.addEventListener("click", handleClickRemoveBtn);
+  });
 }
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,8 +17,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
-  console.log(video);
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
@@ -135,10 +134,25 @@ export const createComment = async (req, res) => {
   if (!video) {
     return res.sendStatus(404);
   }
-  await Comment.create({
+  const comment = await Comment.create({
     text,
     owner: user._id,
     video: id,
   });
-  return res.sendStatus(201);
+  video.comments.push(comment._id);
+  await video.save();
+  return res.status(201).json({ newCommentId: comment._id });
+};
+
+export const deleteComment = async (req, res) => {
+  const { id } = req.params;
+  const comment = await Comment.findById(id);
+  if (!comment) {
+    return res.sendStatus(404);
+  }
+  if (String(comment.owner) !== String(req.session.user._id)) {
+    return res.sendStatus(403);
+  }
+  await Comment.findByIdAndDelete(id);
+  return res.sendStatus(200);
 };
